cd8238dcc4d46d2eeab179eab70b97352547db84
Muhammad Ibrahim <muhdibee@gmail.com>
2020-05-25 14:09:31 +0100

    header and footer

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,37 +1,37 @@
-import React, {Component} from 'react';
-import { Media } from 'reactstrap';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
 
-   constructor(props) {
-       super(props);
+        );
+    }
 
-   }
 
-   render() {
-       const menu = this.props.dishes.map((dish) => {
-           return (
-             <div  className="col-12 col-md-5 m-1">
-               <Card key={dish.id}
-                 onClick={() => this.props.onClick(dish)}>
-                 <CardImg width="100%" src={dish.image} alt={dish.name} />
-                 <CardImgOverlay>
-                     <CardTitle>{dish.name}</CardTitle>
-                 </CardImgOverlay>
-               </Card>
-             </div>
-           );
-       });
+    const Menu = (props) => {
+        const menu =props.dishes.map((dish) => {
+            return (
+                <div  className="col-5 col-md-2 m-1" key= {dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
+                );
+        });
 
-       return (
-           <div className="container">
-               <div className="row">
-                   {menu}
-               </div>
-           </div>
-       );
-      }
-}
+        return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+        );
+
+    }
+
 
 export default Menu;
