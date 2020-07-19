31460424ba2bd2c6eb991be870958f0196e74ad9
Muhammad Ibrahim <muhdibee@gmail.com>
2020-07-19 18:33:23 +0100

    Intro. to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -29,17 +29,17 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return(
-        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
-              promotion={this.state.promotion.filter((promo)=>promo.featured)[0]}
-              leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+        <Home dish={this.props.dishes.filter((dish)=>dish.featured)[0]}
+              promotion={this.props.promotion.filter((promo)=>promo.featured)[0]}
+              leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
         />
       );
     };
 
     const DishWithId = ({match}) => {
       return(
-        <DishDetail dish={this.state.dishes.filter((dish) =>dish.id === parseInt(match.params.dishId, 10))[0]} 
-        comments={this.state.comments.filter((comment) =>comment.dishId === parseInt(match.params.dishId, 10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) =>dish.id === parseInt(match.params.dishId, 10))[0]} 
+        comments={this.props.comments.filter((comment) =>comment.dishId === parseInt(match.params.dishId, 10))} />
       );
     };
 
@@ -48,8 +48,8 @@ class Main extends Component {
         <Header/>
         <Switch>
           <Route path ='/home' component= {HomePage } />
-          <Route path ='/about' component= {() => <About leaders={this.state.leaders}/> } />
-          <Route exact path ='/menu' component= {() => <Menu dishes={this.state.dishes}/> } />
+          <Route path ='/about' component= {() => <About leaders={this.props.leaders}/> } />
+          <Route exact path ='/menu' component= {() => <Menu dishes={this.props.dishes}/> } />
           <Route path ='/menu/:dishId' component= {DishWithId } />
           <Route exact path ='/contactus' component= {Contact} />
           <Redirect to="/home" />
