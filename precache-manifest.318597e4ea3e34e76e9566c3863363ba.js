ef3cf8d0ba580891acf9bb030aa6e64051f365ef
Muhammad Ibrahim <muhdibee@gmail.com>
2020-11-08 21:03:02 +0100

    Updates

---
+++ precache-manifest.318597e4ea3e34e76e9566c3863363ba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364279aceaa0d2b30128429470b74ac9",
+    "url": "/muhdibee/confusion_App/index.html"
+  },
+  {
+    "revision": "e698247e6eb61f1c9b7a",
+    "url": "/muhdibee/confusion_App/static/css/2.2c6a2cfb.chunk.css"
+  },
+  {
+    "revision": "92e5401597409ccd4fd4",
+    "url": "/muhdibee/confusion_App/static/css/main.2de22759.chunk.css"
+  },
+  {
+    "revision": "e698247e6eb61f1c9b7a",
+    "url": "/muhdibee/confusion_App/static/js/2.31e40ab7.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/muhdibee/confusion_App/static/js/2.31e40ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e5401597409ccd4fd4",
+    "url": "/muhdibee/confusion_App/static/js/main.31a6a8b7.chunk.js"
+  },
+  {
+    "revision": "2993547c1fd59b9f7bf3",
+    "url": "/muhdibee/confusion_App/static/js/runtime-main.c6b5404c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/muhdibee/confusion_App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/muhdibee/confusion_App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/muhdibee/confusion_App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/muhdibee/confusion_App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/muhdibee/confusion_App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
